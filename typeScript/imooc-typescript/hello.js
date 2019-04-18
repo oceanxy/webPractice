7738bf141300af6d1b68020ca6eb1a610228b684
Oceanxy <xyzsyx@163.com>
2019-04-18 18:18:48 +0800

    提交ts

---
+++ typeScript/imooc-typescript/hello.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Hello = /** @class */ (function () {
+    function Hello() {
+    }
+    return Hello;
+}());
+exports.Hello = Hello;
