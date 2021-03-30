2ecd75ca7acf4552100b1466920f9a51369357b4
Oceanxy <xyzsyx@163.com>
2021-03-30 15:20:58 +0800

    添加力扣题目：两数相加

--- 算法及面试题/力扣算法题/两数相加（链表）/index.js
+++ 算法及面试题/力扣算法题/两数相加（链表）/index.js
@@ -12,51 +12,81 @@ function ListNode(val, next) {
 const addTwoNumbers = function(l1, l2) {
   if (!l1 || !l2) return
 
-  let node1 = l1
-  let node2 = l2
-  let str1 = ''
-  let str2 = ''
+  //----------------------------解法一：暴力法------------------------------
+  // let node1 = l1
+  // let node2 = l2
+  // let str1 = ''
+  // let str2 = ''
+  //
+  // // 循环求两个加数
+  // while (node1 || node2) {
+  //   if (node1) {
+  //     str1 = `${node1.val}` + str1
+  //
+  //     if (node1.next) {
+  //       node1 = node1.next
+  //     } else {
+  //       node1 = null
+  //     }
+  //   }
+  //
+  //   if (node2) {
+  //     str2 = `${node2.val}` + str2
+  //
+  //     if (node2.next) {
+  //       node2 = node2.next
+  //     } else {
+  //       node2 = null
+  //     }
+  //   }
+  // }
+  //
+  // // 求和
+  // const sum = (BigInt(str1) + BigInt(str2)).toString()
+  // const sumArr = sum.split('')
+  // const lArr = new Array(sumArr.length)
+  //
+  // // 生成链表
+  // sumArr.forEach((number, index) => {
+  //   let next = null
+  //
+  //   if (index) {
+  //     next = lArr[index - 1]
+  //   }
+  //
+  //   lArr[index] = new ListNode(number, next)
+  // })
+  //
+  // return lArr[lArr.length - 1]
 
-  // 循环求两个加数
-  while (node1 || node2) {
-    if (node1) {
-      str1 = `${node1.val}` + str1
+  //----------------------------解法二：数组相同索引位相加------------------------------
+  let node1 = l1, node2 = l2
+  const head = new ListNode()
+  let currentNode = head
+  let carry = false
 
-      if (node1.next) {
-        node1 = node1.next
-      } else {
-        node1 = null
-      }
+  while (
+    node1 || node2 || // 如果node1或node2存在，则相加
+    carry // 如果node1和node2皆不存在，此时判断是否存在上一次的进位，如存在则要在链表末尾添加一个进位结点
+    ) {
+    // 计算 node1、node2 以及 上一次相加得到的进位数 三者之和
+    let sum = (node1?.val ?? 0) + (node2?.val ?? 0) + (carry ? 1 : 0)
+
+    // 进位
+    if (sum >= 10) {
+      sum -= 10
+      carry = true
+    } else {
+      carry = false
     }
 
-    if (node2) {
-      str2 = `${node2.val}` + str2
-
-      if (node2.next) {
-        node2 = node2.next
-      } else {
-        node2 = null
-      }
-    }
+    currentNode.next = new ListNode(sum, null)
+    currentNode = currentNode.next
+    node1 = node1?.next
+    node2 = node2?.next
   }
 
-  // 求和
-  const sum = parseInt(str1) + parseInt(str2)
-  const sumArr = String(sum).split('')
-  const lArr = new Array(sumArr.length)
-
-  // 生成链表
-  sumArr.forEach((number, index) => {
-    let next = null
-
-    if (index) {
-      next = lArr[index - 1]
-    }
-
-    lArr[index] = (new ListNode(number, next))
-  })
-
-  return lArr[lArr.length - 1]
+  return head.next
 }
 
 const listNode5 = new ListNode(4, null)
@@ -65,6 +95,21 @@ const listNode3 = new ListNode(3, listNode4)
 const listNode2 = new ListNode(2, listNode3)
 const listNode1 = new ListNode(2, listNode2)
 const l1 = new ListNode(1, listNode1)
+
+// const listNode13 = new ListNode(1, null)
+// const listNode12 = new ListNode(0, listNode13)
+// const listNode11 = new ListNode(0, listNode12)
+// const listNode10 = new ListNode(0, listNode11)
+// const listNode90 = new ListNode(0, listNode10)
+// const listNode80 = new ListNode(0, listNode90)
+// const listNode70 = new ListNode(0, listNode80)
+// const listNode60 = new ListNode(0, listNode70)
+// const listNode5 = new ListNode(0, listNode60)
+// const listNode4 = new ListNode(0, listNode5)
+// const listNode3 = new ListNode(0, listNode4)
+// const listNode2 = new ListNode(0, listNode3)
+// const listNode1 = new ListNode(0, listNode2)
+// const l1 = new ListNode(1, listNode1)
 
 const listNode9 = new ListNode(3, null)
 const listNode8 = new ListNode(3, listNode9)
