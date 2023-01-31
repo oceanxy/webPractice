d70d3a21c93ed0229ff6e9c17cec437d22bfe33c
Omsber <xyzsyx@163.com>
2023-01-31 10:19:57 +0800

    学习node图片上传并显示

--- node/study/index.js
+++ node/study/index.js
@@ -1,6 +1,7 @@
-var server = require('./server')
-var router = require('./router')
-var requestHandlers = require('./requestHandlers')
+/**
+ * 本代码是在学习 https://www.nodebeginner.org/index-zh-cn.html 的过程中编写的
+ * 主要功能是实现一个简单的图片上传并显示
+ */
 
 var handle = {}
 handle['/'] = requestHandlers.start
